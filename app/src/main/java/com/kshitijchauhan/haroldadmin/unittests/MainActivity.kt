b62b9df37a8bf347a26be10ad6301de96168afbf
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2019-04-04 19:56:42 +0530

    Initialize empty project

---
+++ app/src/main/java/com/kshitijchauhan/haroldadmin/unittests/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.kshitijchauhan.haroldadmin.unittests
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
